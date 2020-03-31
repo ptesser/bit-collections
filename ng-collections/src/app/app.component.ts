08b1e6149664bcd6ec903f35f9b666bf3eec1c86
Paolo Tesser <p.tesser921@gmail.com>
2020-03-31 22:15:20 +0200

    chore(ng-collections): add example for use SwalService

--- ng-collections/src/app/app.component.ts
+++ ng-collections/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { SwalService } from 'src/app/ui/swal/swal.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-collections';
+
+  constructor(
+    private readonly swal: SwalService,
+  ) {
+  }
+
+  showErrorHandler() {
+    this.swal.showError({ title: 'Something went wrong', text: 'Error description' });
+  }
+
+  showSuccessHandler() {
+    this.swal.showSuccess({ title: 'Gret job!' });
+  }
+
+  showWarningHandler() {
+    this.swal.showWarning({ title: 'Attention baby!' });
+  }
+
+  showToastErrorHandler() {
+    this.swal.showErrorToast({ title: 'Something went wrong' });
+  }
+
+  showToastSuccessHandler() {
+    this.swal.showSuccessToast({ title: 'Gret job!' });
+  }
 }
--- ng-collections/src/app/app.module.ts
+++ ng-collections/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { NoItemsModule } from './ui/no-items/no-items.module';
+import { SwalModule } from './ui/swal/swal.module';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +16,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    // Material
+    MatButtonModule,
+    // Bit
     NoItemsModule,
+    SwalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
